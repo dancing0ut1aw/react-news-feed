ec3b57f5adae29a235124a2d2d6d411fadf35681
clairesheek <clairesheek@gmail.com>
2021-03-10 21:04:10 -0600

    branch up-to-date with main

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
-import './App.css';
-import Feed from './components/Feed'
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import SearchBar from "./components/SearchBar";
 
 function App() {
+  const [stories, setStories] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+  const [url, setURL] = useState(
+    "http://hn.algolia.com/api/v1/search?tags=story"
+  );
+
+  let searchInput = React.createRef();
+
+  const handleChange = () => {
+    setInputValue(searchInput.current.value);
+    setURL(
+      `http://hn.algolia.com/api/v1/search?query=${searchInput.current.value}&tags=story`
+    );
+  };
+
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((stories) => setStories(stories.hits));
+  }, [url]);
+
   return (
     <div className="App">
-      NEWS FEED
-      <Feed />
+      <SearchBar
+        stories={stories}
+        searchInput={searchInput}
+        inputValue={inputValue}
+        handleChange={handleChange}
+      />
     </div>
-
-    
   );
 }
 
