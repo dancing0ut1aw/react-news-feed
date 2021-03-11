2924077b9728187e6693889f4e7af34ce732e208
bobbyg211 <robertdg96@gmail.com>
2021-03-10 22:40:43 -0500

    Search type dropdown

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
-import Feed from "./components/Feed"
+import Feed from "./components/Feed";
 import SearchBar from "./components/SearchBar";
-
 
 function App() {
   const [stories, setStories] = useState([]);
+  const [option, setOption] = useState([]);
   const [inputValue, setInputValue] = useState("");
   const [url, setURL] = useState(
     "http://hn.algolia.com/api/v1/search?tags=story"
   );
 
   let searchInput = React.createRef();
+  let selectOptions = React.createRef();
 
   const handleChange = () => {
     setInputValue(searchInput.current.value);
+    setOption(selectOptions.current.value);
     setURL(
-      `http://hn.algolia.com/api/v1/search?query=${searchInput.current.value}&tags=story`
+      `http://hn.algolia.com/api/v1/search?query=${searchInput.current.value}&tags=${selectOptions.current.value}`
     );
   };
 
@@ -29,12 +31,13 @@ function App() {
   return (
     <div className="App">
       <SearchBar
-        stories={stories}
+        option={option}
         searchInput={searchInput}
+        selectOptions={selectOptions}
         inputValue={inputValue}
         handleChange={handleChange}
       />
-      <Feed stories={stories} />
+      <Feed stories={stories} option={option} />
     </div>
   );
 }
--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,23 +1,29 @@
-import React from 'react'
+import React from "react";
 
 // http://hn.algolia.com/api/v1/search?tags=front_page
 
 const Feed = (props) => {
-  const { stories } = props
-
+  const { stories } = props;
 
   return (
-    <ol className="feed"> 
-      {stories.map(story => {
+    <ol className="feed">
+      {stories.map((story) => {
         return (
           <li className="story" key={story.created_at_i}>
-            <h5><a href={story.url}>{story.title}</a> (<a href={story.url}>{story.url}</a>)</h5>
-            <p>{story.points} | {story.author} | {story.created_at.substring(5, 10)}-{story.created_at.substring(0, 4)} | {story.num_comments} comments</p>
+            <h5>
+              <a href={story.url}>{story.title}</a> (
+              <a href={story.url}>{story.url}</a>)
+            </h5>
+            <p>
+              {story.points} | {story.author} |{" "}
+              {story.created_at.substring(5, 10)}-
+              {story.created_at.substring(0, 4)} | {story.num_comments} comments
+            </p>
           </li>
-        )
+        );
       })}
     </ol>
-  )
-}
+  );
+};
 
-export default Feed
+export default Feed;
